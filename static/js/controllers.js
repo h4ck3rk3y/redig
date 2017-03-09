c2cd4880559c6128d508b9353f6af1e61a2a8a38
Gyanendra Mishra <anomaly.the@gmail.com>
2017-03-09 15:24:42 +0800

    tested controllers

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -9,69 +9,87 @@ function AddController($scope, AddTopic, $window)
 		AddTopic.post({topic: topic}, function(data)
 		{
 			$window.location = '/topic/' + data.id;
-		})
+		});
 	}
 }
 
 
-function TopicController($scope, $routeParams, SingleTopic){
-	var topicResults = SingleTopic.get({topic_id: $routeParams.topic_id}, functions(data){
-		$scope.topic = data;
-	});
-}
-
-
-function HomeController($scope, Topic, Upvote, Downvote, $timeout)
+function TopicController($scope, $routeParams, SingleTopic, Upvote, Downvote)
 {
-	var topicResults = Topic.get({}, functions(data){
-		$scope.topics = data.topics;
+	var topicResults = SingleTopic.get({topic_id: $routeParams.topic_id}, function(data){
+		$scope.topics = data;
 	});
 
 	$scope.upvote = function(topic) {
 		Upvote.get({topic_id: topic}, function(data) {
-			var topicResults = Topic.get({}, functions(data){
-				$scope.topics = data.topics;
+			var topicResults = Topic.get({}, function(data){
+				$scope.topics = data;
 			});
 		});
 	}
 
 	$scope.downvote = function(topic) {
 		Downvote.get({topic_id: topic}, function(data) {
-			var topicResults = Topic.get({}, functions(data){
-				$scope.topics = data.topics;
+			var topicResults = Topic.get({}, function(data){
+				$scope.topics = data;
+			});
+		});
+	}
+
+}
+
+
+function HomeController($scope, Topic, Upvote, Downvote, $timeout)
+{
+	var topicResults = Topic.get({}, function(data){
+		$scope.topics = data;
+	});
+
+	$scope.upvote = function(topic) {
+		Upvote.get({topic_id: topic}, function(data) {
+			var topicResults = Topic.get({}, function(data){
+				$scope.topics = data;
+			});
+		});
+	}
+
+	$scope.downvote = function(topic) {
+		Downvote.get({topic_id: topic}, function(data) {
+			var topicResults = Topic.get({}, function(data){
+				$scope.topics = data;
 			});
 		});
 	}
 
 
 	function tick() {
-		var topicResults = Topic.get({}, functions(data){
-			$scope.topics = data.topics;
+		var topicResults = Topic.get({}, function(data){
+			$scope.topics = data;
 		});
 		var timer = $timeout(tick, 3000);
 	}
 
 	tick();
 
-	$scope.$on("destroy", function())
+	$scope.$on("destroy", function()
 	{
 		if(timer) {
 			$timeout.cancel(timer);
 		}
-	}
+	});
 }
 
-function AllTopicsController($scope, $routeParams, $timeout, AllTopics, )
+function AllTopicsController($scope, $routeParams, $timeout, AllTopics, $window)
 {
 	var topicResults = AllTopics.get({page: $routeParams.page}, function(data) {
-        $scope.data = data;
+        $scope.topics = data;
     });
 
 	$scope.upvote = function(topic) {
 		$scope.topics[topic].score+=1;
 		Upvote.get({topic_id: topic}, function(data) {
-			var topicResults = AllTopics.get({page: $routeParams.page}, functions(data){
-				$scope.topics = data.topics;
+			var topicResults = AllTopics.get({page: $routeParams.page}, function(data){
+				$scope.topics = data;
 			});
 		});
 	}
@@ -79,10 +97,21 @@ function AllTopicsController($scope, $routeParams, $timeout, AllTopics, )
 	$scope.downvote = function(topic) {
 		$scope.topics[topic].score+=1;
 		Downvote.get({topic_id: topic}, function(data) {
-			var topicResults = AllTopics.get({page: $routeParams.page}, functions(data){
-				$scope.topics = data.topics;
+			var topicResults = AllTopics.get({page: $routeParams.page}, function(data){
+				$scope.topics = data;
 			});
 		});
 	}
+
+	$scope.next = function()
+	{
+		$window.location = '/all/' + (parseInt($routeParams.page) + 1);
+	}
+
+	$scope.previous = function()
+	{	if($routeParams.page > 1)
+			$window.location = '/all/' + (parseInt($routeParams.page) - 1);
+	}
+
 }
 
